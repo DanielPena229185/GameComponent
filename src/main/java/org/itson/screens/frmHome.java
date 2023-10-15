88e6e3ce28737d3acdf04d7854e062f28dd3d5b1
paulvazquez <paulvazquez@192.168.0.27>
2023-10-15 14:08:58 -0700

    frmHome, imagenes home

--- src/main/java/org/itson/screens/frmHome.java
+++ src/main/java/org/itson/screens/frmHome.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package org.itson.screens;
 
@@ -8,7 +8,7 @@ package org.itson.screens;
  *
  * @author paulvazquez
  */
-public class frmHome extends javax.swing.JPanel {
+public class frmHome extends javax.swing.JFrame {
 
     /**
      * Creates new form frmHome
@@ -26,19 +26,170 @@ public class frmHome extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
+        jPanel2 = new javax.swing.JPanel();
+        btnJoin = new javax.swing.JButton();
+        btnRegresar = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        lblLinea1 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        lbLinea3 = new javax.swing.JLabel();
+        lbNmatch = new javax.swing.JLabel();
+        lbPlayersQuantity = new javax.swing.JLabel();
+        lbGameStatus = new javax.swing.JLabel();
+        lbInfoArea2 = new javax.swing.JLabel();
+        fondo = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        btnJoin.setBackground(new java.awt.Color(255, 255, 255));
+        btnJoin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/home/Unirsetxt.png"))); // NOI18N
+        btnJoin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnJoinActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnJoin, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 610, 320, -1));
+
+        btnRegresar.setBackground(new java.awt.Color(255, 255, 255));
+        btnRegresar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/home/Regresar.png"))); // NOI18N
+        btnRegresar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegresarActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnRegresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(990, 640, 250, 60));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/home/buscarPartida.png"))); // NOI18N
+        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 30, -1, -1));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/home/buscarArea.png"))); // NOI18N
+        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 20, -1, 80));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/home/InformacionPartida.png"))); // NOI18N
+        jPanel2.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 190, -1, -1));
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/home/areaInfo.png"))); // NOI18N
+        jPanel2.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 180, -1, -1));
+
+        lblLinea1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/home/Line 1.png"))); // NOI18N
+        jPanel2.add(lblLinea1, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 280, -1, 200));
+
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/home/nPartida (1).png"))); // NOI18N
+        jPanel2.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 300, -1, -1));
+
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/home/Line 2.png"))); // NOI18N
+        jPanel2.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(690, 280, -1, 200));
+
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/home/Cantidad de jugadores.png"))); // NOI18N
+        jPanel2.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 290, -1, -1));
+
+        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/home/Estado de juego.png"))); // NOI18N
+        jPanel2.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 300, -1, -1));
+
+        lbLinea3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/home/Line 3.png"))); // NOI18N
+        jPanel2.add(lbLinea3, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 360, -1, -1));
+
+        lbNmatch.setBackground(new java.awt.Color(255, 255, 255));
+        lbNmatch.setFont(new java.awt.Font(".SF NS Text", 0, 14)); // NOI18N
+        lbNmatch.setText("Numero");
+        jPanel2.add(lbNmatch, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 400, 110, 30));
+
+        lbPlayersQuantity.setBackground(new java.awt.Color(255, 255, 255));
+        lbPlayersQuantity.setFont(new java.awt.Font(".SF NS Text", 0, 14)); // NOI18N
+        lbPlayersQuantity.setText("Cantidad");
+        jPanel2.add(lbPlayersQuantity, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 400, 110, 30));
+
+        lbGameStatus.setBackground(new java.awt.Color(255, 255, 255));
+        lbGameStatus.setText("Estado");
+        jPanel2.add(lbGameStatus, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 400, 130, 30));
+
+        lbInfoArea2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/home/image 7.png"))); // NOI18N
+        jPanel2.add(lbInfoArea2, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 280, -1, -1));
+        jPanel2.add(fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1280, 720));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 1280, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 720, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
+
+        pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnJoinActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnJoinActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnJoinActionPerformed
+
+    private void btnRegresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegresarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnRegresarActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(frmHome.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(frmHome.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(frmHome.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(frmHome.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new frmHome().setVisible(true);
+            }
+        });
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnJoin;
+    private javax.swing.JButton btnRegresar;
+    private javax.swing.JLabel fondo;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel lbGameStatus;
+    private javax.swing.JLabel lbInfoArea2;
+    private javax.swing.JLabel lbLinea3;
+    private javax.swing.JLabel lbNmatch;
+    private javax.swing.JLabel lbPlayersQuantity;
+    private javax.swing.JLabel lblLinea1;
     // End of variables declaration//GEN-END:variables
 }
