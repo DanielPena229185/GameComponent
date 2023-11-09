9cb203b8bc1aafebff15d84bfc4477a864edccaf
polpoliester <pol_poliester@hotmail.com>
2023-11-09 16:42:00 -0700

    Paneles eliminados de frmMatch. FlowLayout en BoardView

--- src/main/java/org/itson/mvc/board/BoardView.java
+++ src/main/java/org/itson/mvc/board/BoardView.java
@@ -4,10 +4,40 @@
  */
 package org.itson.mvc.board;
 
+import java.awt.FlowLayout;
+import javax.swing.*;
+import java.awt.*;
+import org.itson.domaincomponent.domain.Tile;
+
 /**
  *
  * @author PC
  */
-public class BoardView {
-    
+public class BoardView extends JPanel {
+
+    private BoardModel boardModel;
+    private JPanel mainPanel;
+
+    public BoardView(BoardModel boardModel) {
+        this.boardModel = boardModel;
+    }
+
+    public BoardView() {
+        setSize(1010, 580);
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+        add(mainPanel);
+        setVisible(true);
+    }
+    Tile tile = new Tile();
+
+    public void addTile(Tile tile) {
+        refreshView();
+    }
+
+    public void refreshView() {
+        revalidate();
+        repaint();
+    }
 }
