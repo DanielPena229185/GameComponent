18e3a10c911d82be3b82cb1d5333e328ce5e488c
Jose Hinojosa <74486457+SrTohru@users.noreply.github.com>
2023-11-13 23:47:39 -0700

    avances de back

--- src/main/java/org/itson/classes/DomainBoard.java
+++ src/main/java/org/itson/classes/DomainBoard.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.itson.classes;
 
-/**
- *
- * @author PC
- */
+import org.itson.domaincomponent.domain.Board;
+
 public class DomainBoard {
+
+    private Board board;
+
+    public DomainBoard() {
+        this.board = Board.getInstance();
+    }
     
+    
+    public void addTileBeginning() {
+
+    }
+
+    public void addTileBeginningForMule() {
+
+    }
+
+    public void addTileBeginningNonMule() {
+
+    }
+
+    public void addTileEnd() {
+
+    }
+
+    public void addTileEndForMule() {
+
+    }
+    
+     public void addTileEndForNonMule(){
+        
+    }
+     
+     
+     
+    /*
+    private Board(): Constructor privado que inicializa la lista de baldosas (tiles) como una nueva LinkedList.
+public void addTileBeginning(Tile newTile) throws BoardException: Agrega una baldosa al principio de la lista de baldosas, considerando el tipo de la primera baldosa y su compatibilidad con la nueva baldosa.
+private void addTileBeginningForMule(Tile newTile, Tile firstTile) throws BoardException: Método privado que agrega una baldosa al principio de la lista cuando la primera baldosa es una mula.
+private void addTileBeginningForNonMule(Tile newTile, Tile firstTile) throws BoardException: Método privado que agrega una baldosa al principio de la lista cuando la primera baldosa no es una mula.
+public void addTileEnd(Tile newTile) throws BoardException: Agrega una baldosa al final de la lista de baldosas, considerando el tipo de la última baldosa y su compatibilidad con la nueva baldosa.
+private void addTileEndForMule(Tile newTile, Tile lastTile) throws BoardException: Método privado que agrega una baldosa al final de la lista cuando la última baldosa es una mula.
+private void addTileEndForNonMule(Tile newTile, Tile lastTile) throws BoardException: Método privado que agrega una baldosa al final de la lista cuando la última baldosa no es una mula.
+     */
 }
